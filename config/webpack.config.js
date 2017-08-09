0470cade4759dbbecc842c7fff3f8359f00f0889
huangrunbin <596923143@qq.com>
2017-08-09 22:58:24 +0800

    for template.
    Signed-off-by: huangrunbin <596923143@qq.com>

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -12,7 +13,8 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.json', '.vue'],
     alias: {
-      'vue': 'vue/dist/vue.js'
+      'vue': 'vue/dist/vue.js',
+      '@res': path.resolve(__dirname, 'res')
     }
   },
   module: {
@@ -20,21 +22,55 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['es2015', 'stage-0']
-        }
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
       },
       {
         test: /\.vue$/,
         exclude: /node_modules/,
         loader: 'vue-loader'
+      },
+      {
+        test: /\.attached\.less$/,
+        use: [
+          { loader: 'style-loader/useable' },
+          { loader: 'css-loader' },
+          { loader: 'less-loader' }
+        ]
+      },
+      {
+        test: /\.less$/,
+        exclude: /\.attached\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader' },
+            { loader: 'less-loader' }
+          ]
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 1000
+        }
       }
     ]
   },
   plugins: [
+    new ExtractTextPlugin("style.css"),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './index.html')
     })
-  ]
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "res/images"),
+    host: '0.0.0.0',
+    port: 8081,
+    disableHostCheck: true
+  }
 }
